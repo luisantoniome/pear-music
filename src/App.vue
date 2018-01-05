755b85e918928c449f63a75812088621788042f8
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2018-01-05 11:12:42 -0500

    Initializing project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Pear Music'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
