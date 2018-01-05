c5a64a1f519ffa2426144c0742cd099be23b77b4
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2018-01-05 12:18:45 -0500

    Base search functionality code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,52 @@
 <template lang="pug">
   #app
-    h1 {{ msg }}
+    section.section
+      nav.nav.has-shadow
+        .container
+          .field.has-addons
+            .control.is-expanded
+              input.input.is-large(
+                type="text",
+                placeholder="Search songs",
+                v-model="searchQuery")
+            .control
+              a.button.is-info.is-large(@click="search") Search
+            .control
+              a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}              
+              
+      section.section
+        .container
+          .columns
+            .column(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
 </template>
 
 <script>
+const tracks = [
+  { name: 'Old Friends', artist: 'Jasmine Thompson' },
+  { name: 'Baby Who', artist: 'The Aces' },
+  { name: 'The Cure', artist: 'Lady Gaga' }
+]
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Pear Music'
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Found: ${this.tracks.length}`
+    }
+  },
+
+  methods: {
+    search () {
+      this.tracks = tracks
     }
   }
 }
